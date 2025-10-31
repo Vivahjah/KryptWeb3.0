c83237aab4f110414b17ddbb17e5beac41b3b5de
Mengwa Ugo <mengwa44@yahoo.com>
2025-10-31 02:40:28 +0100

    updated the smart contract

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,38 +3,65 @@ pragma solidity ^0.8.28;
 
 
 
+
 contract Transactions {
-    uint256 public transactionCount; // will keep track of number of transactions
+    uint256 transactionCount;
 
-    event Transfer(address from, address to, uint amount, string message, uint256 timestamp, string keyword);
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
 
-
-//  Structure is a way of saying what properties this object will have
-//  and what type they will be
-// Now I want to make a transaction object, what i the information I want to store in it
-    struct TransferStruct { 
-        address from;
-        address to;
-        uint amount;
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint256 amount;
         string message;
         uint256 timestamp;
         string keyword;
     }
 
-    TransferStruct[] public transactions;
+    TransferStruct[] transactions;
 
-    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+    function addToBlockchain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
         transactionCount += 1;
-        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
 
-        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
     }
-
-    function getAllTransactions() public view returns (TransferStruct[] memory) {
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
         return transactions;
     }
-
-    function getTransactionCount() public view returns (uint) {
+    function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
-}   
+}
