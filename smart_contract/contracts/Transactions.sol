032ce49daf304add11fae63a5e47796db2536b4d
Mengwa Ugo <mengwa44@yahoo.com>
2025-10-31 12:25:42 +0100

    add comments to Transactions.sol file

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -5,8 +5,10 @@ pragma solidity ^0.8.28;
 
 
 contract Transactions {
-    uint256 transactionCount;
+    uint256 transactionCount; // this will keep count of all the transactions
 
+
+    // this event will be emitted whenever a transaction is created
     event Transfer(
         address from,
         address receiver,
@@ -16,6 +18,9 @@ contract Transactions {
         string keyword
     );
 
+
+    // A struct is like an object that shows how the transaction will look like
+    // the data types and the information that each transaction will have
     struct TransferStruct {
         address sender;
         address receiver;
@@ -25,15 +30,18 @@ contract Transactions {
         string keyword;
     }
 
+
+//transactions is an array of TransferStruct meaning it will hold multiple transactions
+//and each transaction will follow the structure defined in TransferStruct
     TransferStruct[] transactions;
 
     function addToBlockchain(
-        address payable receiver,
+        address payable receiver, // payable means that this address can receive Ether
         uint256 amount,
-        string memory message,
+        string memory message, // memory keyword are used for string types in Solidity
         string memory keyword
     ) public {
-        transactionCount += 1;
+        transactionCount += 1; // when a new transaction is added, increment the count
         transactions.push(
             TransferStruct(
                 msg.sender,
@@ -46,18 +54,18 @@ contract Transactions {
         );
 
         emit Transfer(
-            msg.sender,
+            msg.sender, // msg.sender is a global variable that represents the address of the caller
             receiver,
             amount,
             message,
-            block.timestamp,
+            block.timestamp,// timestamp is a global variable that gives the current block timestamp
             keyword
         );
     }
     function getAllTransactions()
         public
         view
-        returns (TransferStruct[] memory)
+        returns (TransferStruct[] memory) 
     {
         return transactions;
     }
